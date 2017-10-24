dd9d33f61b8fd6a797045d795265bb2376e5a270
Ilya <lightmap.shatskiy@gmail.com>
2017-10-24 22:38:22 +0300

    unity 5.5.0p4

--- Assets/Cheats.cs
+++ Assets/Cheats.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,3 +14,4 @@ public class Cheats : MonoBehaviour
 			GameMaster.Instance.Awake ();
 	}
 }
+#endif
